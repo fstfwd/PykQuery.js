03f1d45edbfbf2376b9a672ae8f90ff6b715a0f1
isha-saran <is@pykih.com>
2014-11-21 16:30:30 +0530

    included URL parameters function --- chart or DOM re-render function(minus the priority logic of calling either 'refresh'/'execute'/'call') --- implemeneted the code for checking if the 'div_id' exists on the DOM --- included a new 'filter.js' and 'test_filter_db_query.html' for debugging of the query string generation for filters

---
+++ src/filter.js
@@ -0,0 +1,11 @@
+function filterDbQueryString () {
+	var table_name = "test1data",
+		columns = ["street","city","zip","state","beds",""],
+		query_string = "",
+		query_select = "",
+		query_from = "FROM" + table_name;
+
+	$.getJSON("../data/test1data.json", function(e, data) {
+
+	});
+}
