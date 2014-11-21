f200435886c57ab151935b2df11fa930cbf62891
iGirishhh <kulkarni.girish60@gmail.com>
2014-11-21 19:06:48 +0530

    Do an ajax call to rumi-api to get data

--- src/PykQuery.adapter.rumi.js
+++ src/PykQuery.adapter.rumi.js
@@ -2,9 +2,40 @@ PykQueryAdapterRumi = {}
 
 PykQueryAdapterRumi.init = function(pykquery_json) {
 
-  
-
+  response = ajax_query_pykquery(pykquery_json);
+  //might have to do JSON.parse(response) 
   console.log(pykquery_json);
-  return;
+  return response;
   
+}
+
+var ajax_query_pykquery = function(pykquery_json) {
+  var xmlhttp;
+
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+  } else {
+    // code for IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 ) {
+      if(xmlhttp.status == 200){
+        return xmlhttp.responseText;
+      }
+      else if(xmlhttp.status == 400) {
+        console.error('There was an error 400');
+      }
+      else {
+        console.error('something else other than 200 was returned');
+      }
+    }
+  }
+
+  xmlhttp.open("POST", "filter/get_data", false);
+  xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xmlhttp.send(JSON.stringify(pykquery_json));
+  xmlhttp.send();
 }
