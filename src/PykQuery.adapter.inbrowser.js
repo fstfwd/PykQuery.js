d726c5fa54a34b0ca518daf688c4e0affba89e6f
ronakrrb <rb@pykih.com>
2014-11-22 14:08:39 +0530

    #9 Added aggregation:sum

--- src/PykQuery.adapter.inbrowser.js
+++ src/PykQuery.adapter.inbrowser.js
@@ -1,7 +1,7 @@
 PykQuery.adapter = {};
 PykQuery.adapter.inbrowser = {};
 //PykQuery.adapter.inbrowser.init(pykquery_json);
-PykQuery.adapter.inbrowser.init = function(pykquery){
+PykQuery.adapter.inbrowser.init = function (pykquery){
   var query_object = pykquery,
       raw_data;
   // data whis is used for filtering data is in global_divid_for_raw_data
@@ -10,36 +10,36 @@ PykQuery.adapter.inbrowser.init = function(pykquery){
   console.log(raw_data,query_object);
 
   // function call by adapter from pykquery.js
-  this.call = function() {
+  this.call = function () {
 
     // #1. TEMPORARY CODE TO GET THE DATA FOR TESTING.
     var xmlhttp = new XMLHttpRequest();
     var url = "examples/data/test1data.json";
 
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         raw_data = JSON.parse(xmlhttp.response);
         //console.log(data,pykquery);
         var mode = pykquery.mode;
-        //checking whether filter is exit in query or not 
+        //checking whether filter is exit in query or not
         console.log(query_object.filters);
         if(query_object.filters != undefined){
-          if(query_object.filters.length > 0) { 
+          if(query_object.filters.length > 0) {
             console.log('start filter');
             raw_data = startFilterData(query_object); //call to start filter
 
           }
         }
         switch(mode) {
-            case "aggregation":
-            startAggration(query_object);
+          case "aggregation":
+            startAggregation(query_object);
             break;
-            case "unique":
+          case "unique":
             break;
-            case "datatype":
+          case "datatype":
             break;
-            default:
-            // key: "value", 
+          default:
+            // key: "value",
             console.log('wrong condition type');
         }
 
@@ -50,123 +50,136 @@ PykQuery.adapter.inbrowser.init = function(pykquery){
     // #1 ENDS
 
   }
-  
-  
 
-  var startAggration = function(filterObj){
-    var metrics = filterObj.metrics,
+
+
+  var startAggregation = function (filter_obj){
+    var metrics = filter_obj.metrics,
         local_data;
-     // matrics_column_name = objectkeymatrics;
-    //console.log(matrics["type"])
-    local_data = _.groupBy(raw_data,filterObj.dimensions[0]);
+    // matrics_column_name = objectkeymatrics;
+    local_data = _.groupBy(raw_data,filter_obj.dimensions[0][0]);
+    console.log(filter_obj,local_data)
     //metrices
-    for(var prop in metrics){
-      console.log(metrics[prop]);
-      switch (metrics[prop][0]){
+    for (var prop in metrics) {
+      switch (metrics[prop][0]) {
         case "count":
-          local_data = metricsCountFilter(local_data,prop);//prop is property which is actually a columname 
+          local_data = metricsCount(local_data,prop);//prop is property which is actually a column name
         break;
         case "sum":
-          local_data = metricsSumFilter(local_data,prop);//prop is property which is actually a columname 
+          local_data = metricsSum(local_data,prop);//prop is property which is actually a column name
         break;
+        case "min":
+          local_data = metricsMin(local_data,prop);//prop is property which is actually a column name
+        break;
+        case "max":
+          local_data = metricsMax(local_data,prop);//prop is property which is actually a column name
+        break;
+        case "extent":
+          local_data = metricsExtent(local_data,prop);//prop is property which is actually a column name
+          break;
 
         default:
-        //other cases is remaning 
+        //other cases is remaning
       }
     }
   }
 
-  var metricsCountFilter = function (local_data,column_name){
+  var metricsCount = function (local_data,column_name) {
     var local_filter_array = []
-    _.map(local_data, function(value,key){ 
-       var local_obj = {};
-       local_obj[column_name] =key;
-       local_obj["count"] = value.length;
+    _.map(local_data, function (value,key) {
+      var local_obj = {};
+      local_obj[column_name] = key;
+      local_obj["count"] = value.length;
       local_filter_array.push(local_obj);
     });
     console.log(local_filter_array);
     return local_filter_array;
   }
 
-  var metricsSumFilter = function(local_data,column_name){
-    _.map(local_data, function(values,key){ 
-      var sum;
-        _.map(values, function(value,key){
-          // if()
-          console.log(value[column_name]);
-        });
+  var metricsSum = function (local_data,column_name) {
+    var local_filter_array = []
+    _.map(local_data, function (values,key) {
+      var local_obj = {};
+      local_obj[column_name] = key;
+      local_filter_array.push(local_obj);
+      local_obj["sum"] = _.sum(values, function (value) {
+        return value[column_name];
+      });
+      local_filter_array.push(local_obj);
     });
+    console.log(local_filter_array);
+    return local_filter_array;
   }
 
-  var startFilterData = function(filterObj) {
-    var filters_array =  filterObj.filters;
-    console.log(filterObj.select,filters_array);
+  var startFilterData = function (filter_obj) {
+    var filters_array =  filter_obj.filters;
+    console.log(filter_obj.select,filters_array);
     var len = filters_array.length;
-    for(var i=0;i<len;i++) {
+    for(var i = 0; i < len; i++) {
       //var obj = {columnname:['count']}
-      //checking condition_type of filter exit 
+      //checking condition_type of filter exit
       switch(filters_array[i]["condition_type"]) {
         case "values":
-        console.log('---- value code');
-        return valueFilter(filters_array[i],filterObj.select);
-        break;
+          console.log('---- value code');
+          return valueFilter(filters_array[i],filter_obj.select);
+          break;
         case "range":
-        console.log('---- range code');
-        return rangeFilter(filters_array[i],filterObj.select);
-        break;
+          console.log('---- range code');
+          return rangeFilter(filters_array[i],filter_obj.select);
+          break;
         case "datatype":
-        break;
+          break;
         default:
-        console.log('wrong condition type');
+          console.log('wrong condition type');
       }
     }
 
   }
 
-  var valueFilter = function(filter_obj,columns) {
+  var valueFilter = function (filter_obj,columns) {
     var _in = filter_obj['in'],
         not_in = filter_obj['not_in'],
         column_name = filter_obj['column_name'],
         local_data, col;
-    local_data = _.filter(data ,function(obj){
+    local_data = _.filter(data ,function (obj){
       if(not_in.indexOf(obj[column_name]) < 0){
         return obj;
       }
     });
-    local_data = _.filter(local_data ,function(obj){
+    local_data = _.filter(local_data ,function (obj){
       if(_in.indexOf(obj[column_name]) > -1){
         return obj;
-      } 
+      }
     });
     if(columns.length != 0){
-      local_data = _.map(local_data ,function(obj){ 
+      local_data = _.map(local_data ,function (obj){
         return _.pick(obj,columns);
       });
-    }  
+    }
 
     console.log("value filter completed");
     return local_data;
   }
-  var rangeFilter = function(filter_obj,columns){
+  var rangeFilter = function (filter_obj,columns){
      console.log(columns);
     var min = filter_obj['condition']['min'],
         max = filter_obj['condition']['max'],
         column_name = filter_obj['column_name'],
         local_data, col;
     console.log(min,max);
-    local_data = _.filter(raw_data ,function(obj){
+    local_data = _.filter(raw_data ,function (obj){
       if(obj[column_name] <= max && obj[column_name] >=min){
         return obj;
       }
     });
-    //return perticular columns data 
+    //return perticular columns data
     if(columns.length != 0){
-      local_data = _.map(local_data ,function(obj){ 
+      local_data = _.map(local_data ,function (obj){
         return _.pick(obj,columns);
       });
     }
     console.log("rangeFilter done----");
     return local_data;
   }
-  
+
 }
