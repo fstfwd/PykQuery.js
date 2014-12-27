c15b9bc2c68ace7d5ed71ac8e48f0c67f37f320f
ronakrrb <rb@pykih.com>
2014-12-27 18:45:10 +0530

    REmoved validation from rumi adapter consulting with girish

--- src/PykQuery.adapter.rumi.js
+++ src/PykQuery.adapter.rumi.js
@@ -8,16 +8,15 @@ PykQuery.adapter.rumi.init = function(pykquery_json,rumi_params, queryable_filte
     //console.log(pykquery_json,rumi_params)
     if(rumiParameterValidation(rumi_params)) {
       pykquery_json["filters"] = queryable_filters;
-      var data = { config: pykquery_json,
-        filename: rumi_params["filename"],
-        username: rumi_params["username"],
-        projectname: rumi_params["projectname"] };
+      var data = {
+        config: pykquery_json,
+      };
     } else {
       return false;
     }
 
     $.ajax({
-      url: "http://192.168.0.121:9292/v1/filter/show",
+      url: rumi_api_endpoint + rumi_params + "filter/show",
       data: data, //return  data
       dataType: 'json',
       type: 'POST',
@@ -29,28 +28,5 @@ PykQuery.adapter.rumi.init = function(pykquery_json,rumi_params, queryable_filte
         console.log('Save error.');
       }
     });
-  }
-
-  var rumiParameterValidation = function(params){
-    var util = new PykUtil.init();
-    var util_is_blank = util.isBlank;
-    console.log(util.isBlank(params))
-    if (params == undefined) {
-      console.error('%c[Error - PykQuery] ', 'color: red;font-weight:bold;font-size:14px', "Empty rumi parameter object is not allowed.")
-      return false;
-    }
-    if(util_is_blank(params['filename'])){
-      console.log('%c[Error - PykQuery] ', 'color: red;font-weight:bold;font-size:14px', "filename missing in rumi parameter");
-      return false;
-    }
-    if(util_is_blank(params['username'])){
-      console.log('%c[Error - PykQuery] ', 'color: red;font-weight:bold;font-size:14px', "username missing in rumi parameter");
-      return false;
-    }
-    if(util_is_blank(params['projectname'])){
-      console.log('%c[Error - PykQuery] ', 'color: red;font-weight:bold;font-size:14px', "projectname missing in rumi parameter");
-      return false;
-    }
-    return true;
   }
 }
